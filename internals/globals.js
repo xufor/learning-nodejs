464ed55f68b7095aab3545f8068e451d6e6748ec
Akash Sharma <918527@cognizant.com>
2022-06-14 11:22:16 +0530

    organized files

--- internals/globals.js
+++ internals/globals.js
@@ -1,5 +1,6 @@
 // console.log(global);
 // console.log(global.process);
+// the process global contains all the info about current node process
 
 // console.log(__filename);
 // console.log(__dirname);
